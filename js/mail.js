430315ab0c15f558beca6e627f07ff0e48b49c7d
flashbLack98 <npalakuttige98@icloud.com>
2021-10-13 15:50:33 +0200

    email array

---
+++ js/mail.js
@@ -0,0 +1,34 @@
+let email = prompt("quale è la tua email");
+
+
+const utenti = ["Gino@gmail.com", "Mario@gmail.com", "Paolo@gmail.com", "Carlo@gmail.com"];
+
+for (i = 0; i < utenti.length; i++) {
+
+}
+let mail = utenti;
+console.log(utenti);
+
+if (mail === email) {
+    alert("bene");
+
+} else {
+    alert("male");
+
+}
+
+
+
+
+
+
+
+// CHIEDERE LA MAIL ALL'UTENTE
+
+// CREARE UNA LISTA
+
+//VEDERE SE L'UTENTE È NELLA LISTA
+
+// SE È NELLA LISTA VIENE ACCETTATO 
+
+// SE NON È NELLA LISTA NON VIENE ACCETTATO
